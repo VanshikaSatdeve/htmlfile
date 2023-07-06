5a899789dc8150b8e7c18cf4166c39ab6b095990
Vanshika Satdeve <vsatdeve7@gmail.com>
2023-07-06 17:36:28 +0530

    basic ts file

---
+++ Practice/Basic.ts
@@ -0,0 +1,59 @@
+var a:number=7;
+var b:number=9;
+console.log(a+b);
+
+var h:number=76;
+var i:number=87;
+var j:number=54;
+console.log(h*i*j);
+
+var k:string="Vanshika ";
+var l:string="V. ";
+var m:string="Satdeve";
+console.log(k+l+m);
+
+var n:string[]=["vanshi","snehal","lucky,"];
+var o:string[]=["payal","sakshi"];
+// console.log(n+o);
+
+var p:number=96;
+var q:number=87;
+console.log(p-q);
+
+var r:number=76;
+var s:number=12;
+console.log(r/s);
+
+var t:number[]=[2,4,7,9,13,19,23];
+var b:number=0;
+var i:number;
+for(var i=0;i<=t.length-1;i++){
+    b+=t[i];
+}
+console.log(b);
+
+var v:number[]=[2,4,7,9,13,19,23];
+// var i:number;
+// for(var i=0;i<=t.length-1;i++){
+// }
+console.log(v.length);
+
+
+var t:number[]=[2,4,7,9,13,19,23];
+var b:number=0;
+var i:number;
+for(var i=0;i<=t.length-5;i++){
+    b+=t[i];
+}
+console.log(b);
+
+var t:number[]=[2,4,7,9,13,19,23];
+var b:number=0;
+var i:number;
+for(var i=0;i<=t.length-3;i++){
+    b+=t[i];
+}
+console.log(b);
+
+
+
