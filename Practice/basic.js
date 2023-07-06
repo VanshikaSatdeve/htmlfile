01c483549133483e02113e079dc9445d2baa0ffe
Vanshika Satdeve <vsatdeve7@gmail.com>
2023-07-06 16:59:43 +0530

    basic js file

---
+++ Practice/basic.js
@@ -0,0 +1,54 @@
+var a = 7;
+var b = 9;
+console.log(a + b);
+var c = "65";
+var d = "76";
+console.log(c * d);
+var e = 54;
+var f = 87;
+var g;
+g = e < f;
+console.log(g);
+var h = 76;
+var i = 87;
+var j = 54;
+console.log(h * i * j);
+var k = "Vanshika ";
+var l = "V. ";
+var m = "Satdeve";
+console.log(k + l + m);
+var n = ["vanshi", "snehal", "lucky,"];
+var o = ["payal", "sakshi"];
+// console.log(n+o);
+var p = 96;
+var q = 87;
+console.log(p - q);
+var r = 76;
+var s = 12;
+console.log(r / s);
+var t = [2, 4, 7, 9, 13, 19, 23];
+var b = 0;
+var i;
+for (var i = 0; i <= t.length - 1; i++) {
+    b += t[i];
+}
+console.log(b);
+var v = [2, 4, 7, 9, 13, 19, 23];
+// var i:number;
+// for(var i=0;i<=t.length-1;i++){
+// }
+console.log(v.length);
+var t = [2, 4, 7, 9, 13, 19, 23];
+var b = 0;
+var i;
+for (var i = 0; i <= t.length - 5; i++) {
+    b += t[i];
+}
+console.log(b);
+var t = [2, 4, 7, 9, 13, 19, 23];
+var b = 0;
+var i;
+for (var i = 0; i <= t.length - 3; i++) {
+    b += t[i];
+}
+console.log(b);
